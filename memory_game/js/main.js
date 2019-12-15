a04bbda55c2d95c111b96ccde07040ed4266e220
Aurelie Verrot <aurelie+github@verrot.fr>
2019-12-14 16:21:19 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,32 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+//represents the first card the user flipped and store it 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+//display the 1st card that was just flipped
+console.log("User flipped queen");
+
+//second card flipped and stored
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+//display the second card flipped
+console.log("user flipped king");
+
+//check if the two cards match
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) { 
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+}
