d5af7800a6b304e63e4395775b7baf93cab2a5ca
Aurelie Verrot <aurelie+github@verrot.fr>
2019-12-17 08:55:05 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -34,24 +34,42 @@ var cardsInPlay = [];
 
 //function checking if there is a match between two cards
 function checkForMatch() {
-	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) { 
 			alert("You found a match");
 		} else {
 			alert("Sorry, try again");
 		}
-	}
+	
 };
 
 //function giving user info about the card flipped
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+
+	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	
+	this.setAttribute("src", cards[cardId].cardImage)
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	
 };
 
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	};
+};
+
+createBoard();
+
+
 
